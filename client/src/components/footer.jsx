92df67c12516dcb8d93fb165d0deca5df45ee254
ThiagoIanuch <thiagoianuch@hotmail.com>
2024-04-27 16:11:42 -0300

    Corrigido exportação do componente Footer e alterado nome das classes para padronização

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -2,73 +2,75 @@ import '../css/footer.css'
 
 function Footer() {
     return(
-    <footer>
-        <div id="footer_content">
-            <div id="footer_contacts">
-                <h1>Fale conosco</h1>
-                <div id="footer_numbers">
-                    <a href="" class="footer_link" id="telephone">
-                        <i class="fa-solid fa-phone"></i>
-                        (41)3332-8000
-                    </a>
-                    <a href="" class="footer_link" id="whatsapp">
-                        <i class="fa-brands fa-whatsapp"></i>
-                        (41)98516-3600
-                    </a>
-                    <a href="" class="footer_link" id="whatsapp">
-                        <i class="fa-brands fa-whatsapp"></i>
-                        (41)98516-3602
-                    </a>
+        <footer>
+            <div id="footer-content">
+                <div id="footer-contacts">
+                    <h1>Fale conosco</h1>
+                    <div id="footer-numbers">
+                        <a href="" className="footer-link" id="telephone">
+                            <i className="fa-solid fa-phone"></i>
+                            (41)3332-8000
+                        </a>
+                        <a href="" className="footer-link" id="whatsapp">
+                            <i className="fa-brands fa-whatsapp"></i>
+                            (41)98516-3600
+                        </a>
+                        <a href="" className="footer-link" id="whatsapp">
+                            <i className="fa-brands fa-whatsapp"></i>
+                            (41)98516-3602
+                        </a>
+                    </div>
                 </div>
+                <ul className="footer-list">
+                    <li>
+                        <h3>Onde estamos</h3>
+                    </li>
+                    <li>
+                        <a href="" className="footer-link">Loja 1 - Avenida Presidente Kennedy 410, rebouças - Curitiba</a>
+                    </li>
+                    <li>
+                        <a href="" className="footer-link">Loja 2 - Rua Saturnino Miranda, 84 - Santa Felicidade - Curitiba</a>
+                    </li>
+                </ul>
+                <ul className="footer-list">
+                    <li>
+                        <h3>Peça ja seu orçamento</h3>
+                    </li>
+                    <li>
+                        <a>contate-nos! Buscamos e entregamos seu aparelho </a>
+                    </li>
+                    <li>
+                        <a>Devido a pandemia do COVID-19 estamos atendendo das 8:30 as 17:30 e a avaliação na hora é feita até as 16h</a>
+                    </li>
+                    <li>
+                        <a href="" className="footer-link">
+                            Entre em contato
+                        </a>
+                    </li>
+                </ul>
+                <ul className="footer-list">
+                    <li>
+                        <h3>Tema</h3>
+                    </li>
+                    <li>
+                        <a href="" className="footer-link">
+                            <i className="fa-regular fa-sun"></i> Tema claro
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" className="footer-link">
+                            <i className="fa-regular fa-moon"></i> Tema escuro
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" className="footer-link">
+                            <i className="fa-solid fa-desktop"></i> Tema do sitema
+                        </a>
+                    </li>
+                </ul>
             </div>
-            <ul class="footer-list">
-                <li>
-                    <h3>Onde estamos</h3>
-                </li>
-                <li>
-                    <a href="" class="footer_link">Loja 1 - Avenida Presidente Kennedy 410, rebouças - Curitiba</a>
-                </li>
-                <li>
-                    <a href="" class="footer_link">Loja 2 - Rua Saturnino Miranda, 84 - Santa Felicidade - Curitiba</a>
-                </li>
-            </ul>
-            <ul class="footer-list">
-                <li>
-                    <h3>Peça ja seu orçamento</h3>
-                </li>
-                <li>
-                    <a>contate-nos! Buscamos e entregamos seu aparelho </a>
-                </li>
-                <li>
-                    <a>Devido a pandemia do COVID-19 estamos atendendo das 8:30 as 17:30 e a avaliação na hora é feita até as 16h</a>
-                </li>
-                <li>
-                    <a href="" class="footer_link">
-                        Entre em contato
-                    </a>
-                </li>
-            </ul>
-            <ul class="footer-list">
-                <li>
-                    <h3>Tema</h3>
-                </li>
-                <li>
-                    <a href="" class="footer_link">
-                        <i class="fa-regular fa-sun"></i> Tema claro
-                    </a>
-                </li>
-                <li>
-                    <a href="" class="footer_link">
-                        <i class="fa-regular fa-moon"></i> Tema escuro
-                    </a>
-                </li>
-                <li>
-                    <a href="" class="footer_link">
-                        <i class="fa-solid fa-desktop"></i> Tema do sitema
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </footer>
+        </footer>
     );
 }
+
+export default Footer
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,9 +1,9 @@
 import Menu from '../components/menu.jsx'
 import Hero from '../components/hero.jsx'
 import Announcements from '../components/announcements.jsx'
-import Footer from '../components/footer.jsx'
-import Services from '../components/services.jsx'
 
+import Services from '../components/services.jsx'
+import Footer from '../components/footer.jsx'
 
 function Index() {
     return (
@@ -11,9 +11,9 @@ function Index() {
             <Menu></Menu>
             <Hero></Hero>
             <Announcements></Announcements>
-            <Footer></Footer>
+
             <Services></Services>
-            
+            <Footer></Footer>
         </>
     )
 }

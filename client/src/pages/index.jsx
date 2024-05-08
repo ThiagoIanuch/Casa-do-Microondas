085b7cbc531bc0cbe045fcc0691a1969fc826ab0
le-leodasilva <leandroleopoldino05@gmail.com>
2024-05-07 21:03:45 -0300

    Criar componente brands

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,21 +1,22 @@
-import Menu from '../components/menu.jsx'
-import Hero from '../components/hero.jsx'
-import Announcements from '../components/announcements.jsx'
-
-import Services from '../components/services.jsx'
-import Footer from '../components/footer.jsx'
+import React from 'react';
+import Menu from '../components/menu.jsx';
+import Hero from '../components/hero.jsx';
+import Announcements from '../components/announcements.jsx';
+import Brands from '../components/brands.jsx';
+import Services from '../components/services.jsx';
+import Footer from '../components/footer.jsx';
 
 function Index() {
     return (
         <>
-            <Menu></Menu>
-            <Hero></Hero>
-            <Announcements></Announcements>
-
-            <Services></Services>
-            <Footer></Footer>
+            <Menu />
+            <Hero />
+            <Announcements />
+            <Brands /> {/* Renderiza o Slider */}
+            <Services />
+            <Footer />
         </>
-    )
+    );
 }
 
-export default Index
+export default Index;

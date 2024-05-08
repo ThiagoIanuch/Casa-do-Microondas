ded5690e5154c7c92982eaa4a5f429ffda8bc131
GabrielDBarbieri <gabrieldomingos457@gmail.com>
2024-05-08 12:51:56 -0300

    Modelo Logico do banco de dados

---
+++ client/src/pages/location.jsx
@@ -0,0 +1,22 @@
+
+
+    import Menu from '../components/menu.jsx'
+import Footer from '../components/footer.jsx'
+
+function Location() {
+    return (
+        <>
+        <Menu></Menu>
+        
+        
+
+
+
+
+
+        <Footer></Footer>
+        </>
+    )
+}
+
+export default Location

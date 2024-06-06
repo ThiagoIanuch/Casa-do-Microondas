2ae18ae767355d35306fe781a002fa1712cab629
le-leodasilva <leandroleopoldino05@gmail.com>
2024-06-05 21:37:27 -0300

    Adicionar rotas no index do servidor

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,23 @@
-// Importar as bibliotecas e rotas
+// Importar as bibliotecas
 const express = require('express');
 const cors = require('cors');
 
+// Importar as rotas
 const userRoutes = require('./routes/userRoutes.js');
 const productRoutes = require('./routes/productRoutes.js');
+const brandRoutes = require('./routes/brandRoutes.js');
+const announcementRoutes = require('./routes/announcementRoutes.js');
 
-// Iniciar o servidor
+// Iniciar o servidor e chamar as rotas
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/user', userRoutes);
 app.use('/api/product', productRoutes);
+app.use('/api/brand',brandRoutes);
+app.use('/api/announcement',announcementRoutes);
+
 
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080");

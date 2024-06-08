7d98926f413ea2b1d6bf8d7a0276a7ca69bf01b3
le-leodasilva <leandroleopoldino05@gmail.com>
2024-06-08 00:10:05 -0300

    atualização do index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,7 @@ const userRoutes = require('./routes/userRoutes.js');
 const productRoutes = require('./routes/productRoutes.js');
 const brandRoutes = require('./routes/brandRoutes.js');
 const announcementRoutes = require('./routes/announcementRoutes.js');
+const contactRoutes = require('./routes/contactRoutes.js')
 
 // Iniciar o servidor e chamar as rotas
 const app = express();
@@ -17,7 +18,7 @@ app.use('/api/user', userRoutes);
 app.use('/api/product', productRoutes);
 app.use('/api/brand',brandRoutes);
 app.use('/api/announcement',announcementRoutes);
-
+app.use('/api/contact',contactRoutes);
 
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080");
